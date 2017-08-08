06d73130961cdd1cece32f67b23be6cac4b22ae0
Maxime Rainville <maxime@webtorque.co.nz>
2017-08-08 17:39:38 +1200

    NIT-67 Try to auto add the MemberNotificationExtension to Member.

--- _config.php
+++ _config.php
@@ -1,6 +1,2 @@
 <?php
-var_dump(Director::isTest());
-die();
-if (Director::isTest()) {
-    Member::add_extension('MemberNotificationExtension');
-}
+Member::add_extension('MemberNotificationExtension');
